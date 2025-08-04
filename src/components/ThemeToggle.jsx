154d50e71575d065c23c8dc6065cacafdaebceaf
Barin00 <babayevriad2005@gmail.com>
2025-08-03 22:01:43 -0500

    Default theme to dark mode in ThemeToggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -7,12 +7,15 @@ export const ThemeToggle = () => {
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("theme", "light");
+
+    if (storedTheme === "light") {
+      document.documentElement.classList.remove("dark");
       setIsDarkMode(false);
+    } else {
+      // Defaults to dark mode if no theme is set
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
     }
   }, []);
 
